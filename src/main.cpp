bcc0171ae69848e138d113766e354d0ff0149490
Anurag Chaudhary <anurag.choudhary_cs.aiml24@gla.ac.in>
2026-02-03 09:12:34 +0530

    add pin in cnfiguration in ultrasonic sensor

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 #include <Arduino.h>
+#define trioPin 9
+#define echoPin 10
 void setup() {
 // write your initialization code here
+    pinMode(trioPin, OUTPUT);
+    pinMode(echoPin, INPUT);
 }
 
 void loop() {
